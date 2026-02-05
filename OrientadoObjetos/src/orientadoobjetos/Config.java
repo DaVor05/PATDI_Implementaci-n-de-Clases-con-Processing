1b74a66c1dc2a870926174bb279791196d6c3a02
DaVor05 <davidvid1201@gmail.com>
2026-02-05 10:23:43 -0800

    Delete OrientadoObjetos directory

--- OrientadoObjetos/src/orientadoobjetos/Config.java
+++
@@ -1,9 +0,0 @@
-package orientadoobjetos;
-
-public class Config {
-    
-    public static final int ANCHO = 600;
-    public static final int ALTO = 600;
-    public static final int COLOR_FONDO = 600;
-            
-}
